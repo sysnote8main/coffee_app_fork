d9abc472341d47179db819d009294d8ceb161d3e
nakamu <oyukita56@gmail.com>
2023-09-23 21:13:53 +0900

    cors通るようパスを修正

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins ENV["FRONT_PORT"] || ""
+      origins "localhost:5173" || ""
 
       resource '*',
         headers: :any,
